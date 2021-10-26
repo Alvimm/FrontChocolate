b3abc2efaf6227e797cb793ea8b1277cb14911b5
Alvimm <filipe2012alvim@gmail.com>
2021-10-26 01:22:28 -0300

    Front commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+const imagem = 'img';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={imagem} alt="logo" id={imagem} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
