4f93a1cb09388d05f92809a5f9b27291e8e71052
Alvimm <filipe2012alvim@gmail.com>
2021-10-28 00:49:10 -0300

    Update Front

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-
 // eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
   padding: 30px;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GlobalStyle from '../../styles/global';
 import { Container } from '../../components/container';
+import { ChocolateList } from './style';
 import chocolateImg from '../../assets/images/chocolate.svg';
+import api from '../../services/api';
 
 function App() {
+  const [infos, setInfos] = useState([]);
+
+  useEffect(async () => {
+    const response = await api.get('/posts');
+
+    setInfos(response.data);
+  }, []);
+
   return (
     <div className="App">
       <GlobalStyle />
       <Container>
         <img src={chocolateImg} alt="Imagem de chocolate" />
         <h1>Chocolates</h1>
+
+        <ChocolateList>
+          {infos.map((info) => (
+            <li key={info.id}>
+              <div>
+                <img src={chocolateImg} alt="Imagem de chocolte" />
+                <div>
+                  <h2>{info.title}</h2>
+                  <p>{info.id}</p>
+                  <p>{info.body}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ChocolateList>
       </Container>
     </div>
   );
